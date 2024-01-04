e5c3b60be12ca87cb459c63af888ef72e6432654
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-01-04 18:58:35 -0300

    Example of how handling error in Rust command line

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,18 +1,9 @@
-/// Errors for the program
-#[derive(Debug)]
-enum Error {
-    PathIsRequired,
-}
-
-
-fn main() -> Result<(), Error> {
-    let path = match std::env::args().nth(2) {
-        Some(p) => p,
-        None => return Err(Error::PathIsRequired),
-    };
-        
-
-    dbg!(path);
-
-    Ok(())
+// I think this is the best way to return an error in command line 
+// I don't know why some people use `fn main() -> Result<(), Error>`.
+    
+fn main() {
+    let path = std::env::args().nth(2).ok_or_else(|| {
+        eprintln!("ERROR: you must pass the path directory");
+        std::process::exit(1);
+    });
 }
